10fcf0fa548cb26e4eea515e0d10aaa6879bade0
likhithasundunagunta <111753026+likhithasundunagunta@users.noreply.github.com>
2024-11-12 19:42:12 +0530

    Create home_screen.dart

---
+++ home_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'details_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List movies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMovies();
+  }
+
+  Future<void> fetchMovies() async {
+    final response = await http.get(Uri.parse('https://api.tvmaze.com/search/shows?q=all'));
+    if (response.statusCode == 200) {
+      setState(() {
+        movies = json.decode(response.body);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: GestureDetector(
+          onTap: () => Navigator.pushNamed(context, '/search'),
+          child: Text('Search Movies'),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (context, index) {
+          final movie = movies[index]['show'];
+          return ListTile(
+            leading: Image.network(movie['image']?['medium'] ?? ''),
+            title: Text(movie['name']),
+            subtitle: Text(movie['summary'] ?? '', maxLines: 2),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailsScreen(movie: movie)),
+              );
+            },
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        onTap: (index) {
+          if (index == 1) {
+            Navigator.pushNamed(context, '/search');
+          }
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        ],
+      ),
+    );
+  }
+}
